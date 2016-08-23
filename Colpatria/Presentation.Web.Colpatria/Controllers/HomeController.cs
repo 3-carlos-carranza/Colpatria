faa1ecc0c706029f0ab2330eeb68091627907bd3
BANLINEA\itamar.aguirre <itamar.aguirre@banlinea.com>
2016-08-23 15:42:43 -0500

    Added presentation web Colpatria project Mvc

---
+++ Colpatria/Presentation.Web.Colpatria/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Presentation.Web.Colpatria.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

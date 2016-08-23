a2a469f55475789fe0da201e09354de6c138b5b9
BANLINEA\itamar.aguirre <itamar.aguirre@banlinea.com>
2016-08-23 17:38:38 -0500

    Added dependency injection factory

---
+++ Colpatria/Crusscutting.DependencyInjectionFactory/ContainerInitializer.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Practices.Unity;
+
+namespace Crusscutting.DependencyInjectionFactory
+{
+    public static class ContainerInitializer
+    {
+        public static void InitializeContainer(this IUnityContainer container)
+        {
+            
+        }
+    }
+}
---
+++ Colpatria/Crusscutting.DependencyInjectionFactory/Factory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Unity;
+
+namespace Crusscutting.DependencyInjectionFactory
+{
+    public static class Factory
+    {
+        private static readonly DiContainer Container;
+
+
+        public static TServicio Resolver<TServicio>()
+        {
+            return Container.Current.Resolve<TServicio>();
+        }
+
+        static Factory()
+        {
+            Container = new DiContainer();
+        }
+    }
+}

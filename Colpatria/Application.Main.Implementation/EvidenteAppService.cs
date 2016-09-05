fa35dd5c1b8040ebfe9430bf4ab912b004783e0f
BANLINEA\itamar.aguirre <itamar.aguirre@banlinea.com>
2016-09-05 15:10:24 -0500

    Added service evidente

---
+++ Colpatria/Application.Main.Implementation/EvidenteAppService.cs
@@ -0,0 +1,31 @@
+﻿using Application.Main.Definition;
+using Core.Entities.Evidente;
+using Core.GlobalRepository.Evidente;
+
+namespace Application.Main.Implementation
+{
+     public class EvidenteAppService : IEvidenteAppService
+     {
+         private readonly IEvidenteRepository _evidenteRepository;
+
+         public EvidenteAppService(IEvidenteRepository evidenteRepository)
+         {
+             _evidenteRepository = evidenteRepository;
+         }
+
+         public AnswerResponse AnswerQuestions(AnswerSettings settings)
+         {
+             return _evidenteRepository.AnswerQuestions(settings);
+         }
+
+         public QuestionsResponse GetQuestions(QuestionsSettings settings)
+         {
+             return _evidenteRepository.GetQuestions(settings);
+         }
+
+         public ValidationResponse Validate(ValidateUserSettings settings)
+         {
+             return _evidenteRepository.Validate(settings);
+         }
+    }
+}

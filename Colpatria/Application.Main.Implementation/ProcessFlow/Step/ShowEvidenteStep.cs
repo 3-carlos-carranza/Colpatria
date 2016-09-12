3fba5a1511a3352cbcc0da8cf10d6e0eb3b7381b
BANLINEA\itamar.aguirre <itamar.aguirre@banlinea.com>
2016-09-12 12:07:12 -0500

    Added ShowEvidenteStep

---
+++ Colpatria/Application.Main.Implementation/ProcessFlow/Step/ShowEvidenteStep.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Main.Definition.ProcessFlow.Api.ProcessFlows;
+using Application.Main.Definition.ProcessFlow.Api.ProcessFlows.Response;
+using Application.Main.Definition.ProcessFlow.Api.Steps;
+
+namespace Application.Main.Implementation.ProcessFlow.Step
+{
+    public class ShowEvidenteStep : BaseStep
+    {
+        public ShowEvidenteStep(IProcessFlowStore store) : base(store)
+        {
+        }
+
+        public override Task<IProcessFlowResponse> Advance(IProcessFlowArgument argument)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Task<IProcessFlowResponse> AdvanceAsync(IProcessFlowArgument argument, CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

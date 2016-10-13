78318f0081d11ce8003321a85b35bc01a3b37f01
BANLINEA\itamar.aguirre <itamar.aguirre@banlinea.com>
2016-10-13 15:24:25 -0500

    Added Application insights

---
+++ Colpatria/Presentation.Web.Colpatria/App_Start/FilterConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using Banlinea.Framework.Logging.Insights.Mvc;
+
+namespace Presentation.Web.Colpatria
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new AiHandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
+        }
+
+    }
+}

c3cc3e832208154e0b1c70a6aec87c7170fe3ce3
CamiloBernal <camilo.bernal@banlinea.com>
2016-10-13 15:34:56 -0500

    Fix vs warning CS0114 for Static Code Analysis

--- Colpatria/Core.Entities/Process/Page.cs
+++ Colpatria/Core.Entities/Process/Page.cs
@@ -16,14 +16,7 @@ namespace Core.Entities.Process
 {
     public class Page : PageFlow
     {
-        public int Id { get; set; }
         public int ProcessId { get; set; }
-        public string Name { get; set; }
-        public int Order { get; set; }
-        public bool IsVisible { get; set; }
-        public string NameAlias { get; set; }
-        public bool Enable { get; set; }
-
         public virtual Process Process { get; set; }
         public virtual ICollection<Section> Section { get; set; } = new HashSet<Section>();
     }

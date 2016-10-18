22ef3a58d2a1878b7007b5f02f93ac667af1ac9a
CamiloBernal <camilo.bernal@banlinea.com>
2016-10-18 17:52:16 -0500

    Fix VS Warning: 'CA2210: Assemblies should have valid strong names' in Static Code Analysis

---
+++ Colpatria/Data.Common.Helpers.EF.Extensions/StoredProcedureParserHelper.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Data.Common.Helpers.EF.Extensions
+{
+    internal class StoredProcedureParserHelper
+    {
+        public string GetParameterName(PropertyInfo propertyInfo)
+        {
+            var rValue = propertyInfo.Name;
+
+            // grab the name of the parameter from the attribute, if it's defined.
+            var attribute = Attributes.GetAttribute<StoredProcedureParameterAttribute>(propertyInfo);
+            if (!string.IsNullOrEmpty(attribute?.ParameterName))
+                rValue = attribute.ParameterName;
+
+            return rValue;
+        }
+
+        public string GetUserDefinedTableType(PropertyInfo propertyInfo)
+        {
+            var collectionType = GetCollectionType(propertyInfo.PropertyType);
+            var attribute = Attributes.GetAttribute<UserDefinedTableTypeAttribute>(collectionType);
+
+            if (attribute == null)
+                throw new InvalidOperationException(
+                    $"{propertyInfo.PropertyType} has not been decorated with UserDefinedTableTypeAttribute.");
+
+            return attribute.Name;
+        }
+
+        public Type GetCollectionType(Type type)
+        {
+            if (type.IsGenericType)
+            {
+                foreach (var interfaceType in type.GetInterfaces())
+                {
+                    if (interfaceType.GetGenericTypeDefinition() == typeof(IList<>))
+                    {
+                        return interfaceType.GetGenericArguments()[0];
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public object GetUserDefinedTableValue(PropertyInfo propertyInfo, object storedProcedure)
+        {
+            var enumerableType = GetCollectionType(propertyInfo.PropertyType);
+            var propertyValue = propertyInfo.GetValue(storedProcedure, null);
+
+            var generator = new UserDefinedTableGenerator(enumerableType, propertyValue);
+
+            var table = generator.GenerateTable();
+
+            return table;
+        }
+
+        public bool IsUserDefinedTableParameter(PropertyInfo propertyInfo)
+        {
+            var collectionType = GetCollectionType(propertyInfo.PropertyType);
+
+            return collectionType != null;
+        }
+
+        public bool ParameterIsMandatory(StoredProcedureParameterOptions options)
+        {
+            return (options & StoredProcedureParameterOptions.Mandatory) ==
+                   StoredProcedureParameterOptions.Mandatory;
+        }
+    }
+}

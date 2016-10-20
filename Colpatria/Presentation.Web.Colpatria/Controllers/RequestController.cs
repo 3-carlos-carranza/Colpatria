fe79ea5ee21e099ff2cf4f5e8a54400eb77f4777
BANLINEA\itamar.aguirre <itamar.aguirre@banlinea.com>
2016-10-19 19:38:13 -0500

    Merge

--- Colpatria/Presentation.Web.Colpatria/Controllers/RequestController.cs
+++ Colpatria/Presentation.Web.Colpatria/Controllers/RequestController.cs
@@ -92,7 +92,7 @@ namespace Presentation.Web.Colpatria.Controllers
                     _userAppService.GetValidExecutionByUserAndProduct(user.Id, (int) Session["Product"]);
 
                 if (currentSectionId != 0)
-                    return View("ContinueRequest", new UserViewModel
+                    return View("ContinueRequest", new UserViewModel { 
                         FirstName = "Wilmar",
                         ProductId = Convert.ToInt32((int) Session["Product"]),
                         CurrentSectionId = currentSectionId

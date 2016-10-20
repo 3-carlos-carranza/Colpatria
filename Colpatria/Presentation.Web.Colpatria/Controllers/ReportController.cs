4d557c071c5a3b2351d6334a163559c5e351b6e6
Jeysson Ramirez <jeysson.ramirez@banlinea.com>
2016-10-20 14:45:06 -0500

    login report

---
+++ Colpatria/Presentation.Web.Colpatria/Controllers/ReportController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Presentation.Web.Colpatria.Controllers
+{
+    public class ReportController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginReportModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            Session["login"] = true;
+            return RedirectToAction("Download");
+        }
+        [HttpGet]
+        public ActionResult Download()
+        {
+            var session = Session["login"];
+            if (session == null)
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Download(DownloadReportModel model)
+        {
+            var session = Session["login"];
+            if (session == null)
+            {
+                return RedirectToAction("Login");
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            string filename = "File.pdf";
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "/Path/To/File/" + filename;
+            byte[] filedata = System.IO.File.ReadAllBytes(filepath);
+            string contentType = MimeMapping.GetMimeMapping(filepath);
+
+            var cd = new System.Net.Mime.ContentDisposition
+            {
+                FileName =$"Base {model.StartDate} - {model.EndDate} .xls",
+                Inline = true,
+            };
+
+            Response.AppendHeader("Content-Disposition", cd.ToString());
+
+            return File(filedata, contentType);
+        }
+    }
+
+    public class LoginReportModel
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+    public class DownloadReportModel
+    {
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime EndDate { get; set; }
+        
+    }
+}
